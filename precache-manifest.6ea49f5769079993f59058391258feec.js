ff9b8712273cd61798a3654746f715be1692037f
Igbinoba Ebenezer <igbinobaebenezer@gmail.com>
2018-12-10 13:53:22 +0100

    Updates

---
+++ precache-manifest.6ea49f5769079993f59058391258feec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/EBEN4REAL/burger-builder-app.git/static/media/127 burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "72e033f2b5a667b92d7b",
+    "url": "/EBEN4REAL/burger-builder-app.git/static/js/runtime~main.72e033f2.js"
+  },
+  {
+    "revision": "fad4874022046096689f",
+    "url": "/EBEN4REAL/burger-builder-app.git/static/js/main.fad48740.chunk.js"
+  },
+  {
+    "revision": "22e0c3f80a02bc4d2805",
+    "url": "/EBEN4REAL/burger-builder-app.git/static/js/1.22e0c3f8.chunk.js"
+  },
+  {
+    "revision": "fad4874022046096689f",
+    "url": "/EBEN4REAL/burger-builder-app.git/static/css/main.0677aa17.chunk.css"
+  },
+  {
+    "revision": "a2bd3c992aec2db7b1e676f2462881c5",
+    "url": "/EBEN4REAL/burger-builder-app.git/index.html"
+  }
+];
