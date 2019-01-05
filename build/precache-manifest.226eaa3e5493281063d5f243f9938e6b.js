f28503cada3f9287080346490f9881c59d0ebbe6
Igbinoba Ebenezer <ebenezer.igbinoba@stu.cu.edu.ng>
2019-01-05 17:08:39 +0100

    some commit

---
+++ build/precache-manifest.226eaa3e5493281063d5f243f9938e6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/EBEN4REAL/burger-builder-app.git/static/media/127 burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "72e033f2b5a667b92d7b",
+    "url": "/EBEN4REAL/burger-builder-app.git/static/js/runtime~main.72e033f2.js"
+  },
+  {
+    "revision": "a51d455d920f655a3b51",
+    "url": "/EBEN4REAL/burger-builder-app.git/static/js/main.a51d455d.chunk.js"
+  },
+  {
+    "revision": "efe65bb3a67b2e09c119",
+    "url": "/EBEN4REAL/burger-builder-app.git/static/js/1.efe65bb3.chunk.js"
+  },
+  {
+    "revision": "a51d455d920f655a3b51",
+    "url": "/EBEN4REAL/burger-builder-app.git/static/css/main.b17d58bd.chunk.css"
+  },
+  {
+    "revision": "def70d0efe9b95f7fdf6ebac0e2afa3d",
+    "url": "/EBEN4REAL/burger-builder-app.git/index.html"
+  }
+];
